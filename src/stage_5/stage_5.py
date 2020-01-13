398aded937a06bfc15c28f893a1f25b9f95efd3e
KentJacobs <kent.jacobs@canada.ca>
2020-01-13 16:23:35 -0500

    initial stage 5 commit

---
+++ src/stage_5/stage_5.py
@@ -0,0 +1,54 @@
+import click
+import logging
+import os
+import sys
+from sqlalchemy import *
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import helpers
+
+
+# Set logger.
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S"))
+logger.addHandler(handler)
+
+
+class Stage:
+    """Defines an NRN stage."""
+
+    def __init__(self, source):
+        self.stage = 5
+        self.source = source.lower()
+
+        # Configure and validate input data path.
+        self.data_path = os.path.abspath("../../data/interim/{}.gpkg".format(self.source))
+        if not os.path.exists(self.data_path):
+            logger.exception("Input data not found: \"{}\".".format(self.data_path))
+            sys.exit(1)
+            
+    def execute(self):
+        """Executes an NRN stage."""
+
+
+@click.command()
+@click.argument("source", type=click.Choice("ab bc mb nb nl ns nt nu on pe qc sk yt parks_canada".split(), False))
+def main(source):
+    """Executes an NRN stage."""
+
+    try:
+
+        with helpers.Timer():
+            stage = Stage(source)
+            stage.execute()
+
+    except KeyboardInterrupt:
+        logger.exception("KeyboardInterrupt: exiting program.")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
