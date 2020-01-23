7c330676aa7b0b861f91485bc80f07644d44caad
KentJacobs <kent.jacobs@canada.ca>
2020-01-23 10:41:51 -0500

    alter ogr2ogr subprocess to reproject entire directory

--- src/stage_5/stage_5.py
+++ src/stage_5/stage_5.py
@@ -1,5 +1,6 @@
 import click
 import geopandas as gpd
+import glob
 import numpy as np
 import logging
 import os
@@ -38,34 +39,35 @@ class Stage:
             sys.exit(1)
 
     def load_gpkg(self):
-        """Loads input GeoPackage layers into dataframes."""
+        """Loads input GeoPackage layers into data frames."""
 
         logger.info("Loading Geopackage layers.")
 
         self.dframes = helpers.load_gpkg(self.data_path)
 
     def dl_latest_vintage(self):
-        """Downloads the latest provincial NRN dataset from Open Maps/FGP."""
+        """Downloads the latest provincial NRN data set from Open Maps/FGP."""
 
-        # Download latest vintage dataset.
+        # Download latest vintage data set.
         logger.info("Downloading latest provincial dataset.")
         vintage = "https://geoprod.statcan.gc.ca/nrn_rrn/nb/NRN_RRN_NB_9_0_SHP.zip"
         urllib.request.urlretrieve(vintage, '../../data/raw/vintage.zip')
         with zipfile.ZipFile("../../data/raw/vintage.zip", "r") as zip_ref:
             zip_ref.extractall("../../data/raw/vintage")
 
-        # Transform administrative boundary file to GeoPackage layer with crs EPSG:4617.
+        # Transform latest vintage into crs EPSG:4617.
         logger.info("Transforming latest provincial dataset.")
         try:
-            subprocess.run("ogr2ogr -f GPKG ../../data/raw/vintage/vintage.gpkg "
-                           "../../data/raw/vintage/NRN_RRN_NB_9_0_SHP/NRN_NB_9_0_SHP_en/NRN_NB_9_0_ROADSEG.shp -t_srs EPSG:4617 ")
+            subprocess.run('ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:4617 '
+                           '../../data/raw/vintage/NRN_RRN_NB_9_0_SHP/NRN_NB_9_0_SHP_en/4617 '
+                           '../../data/raw/vintage/NRN_RRN_NB_9_0_SHP/NRN_NB_9_0_SHP_en')
         except subprocess.CalledProcessError as e:
             logger.exception("Unable to transform data source to EPSG:4617.")
             logger.exception("ogr2ogr error: {}".format(e))
             sys.exit(1)
 
         logger.info("Reading latest provincial dataset.")
-        self.old = gpd.read_file("../../data/raw/vintage/vintage.gpkg")
+        self.vintage_roadseg = gpd.read_file("../../data/raw/vintage/NRN_RRN_NB_9_0_SHP/NRN_NB_9_0_SHP_en/4617/NRN_NB_9_0_ROADSEG.shp")
 
     def roadseg_equality(self):
         """Checks if roadseg features have equal geometry."""
@@ -80,11 +82,11 @@ class Stage:
 
         logger.info("Checking for road segment geometry equality.")
         # Returns True or False to a new column if geometry is equal.
-        self.dframes["roadseg"]["equals"] = self.dframes["roadseg"].geom_equals(self.dframes["roadseg"])
+        self.dframes["roadseg"]["equals"] = self.dframes["roadseg"].geom_equals(self.vintage_roadseg)
 
         logger.info("Writing test GPKG.")
         # self.dframes["roadseg"].to_file("../../data/interim/nb_test.gpkg", layer="new_equal", driver="GPKG")
-        helpers.export_gpkg({"new_equal": self.dframes["roadseg"]}, self.data_path)
+        helpers.export_gpkg({"roadseg_equal": self.dframes["roadseg"]}, self.data_path)
 
     def execute(self):
         """Executes an NRN stage."""
