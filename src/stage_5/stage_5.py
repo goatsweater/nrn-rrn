9899a8ff2735fb922dc868780e18e74b09baec20
KentJacobs <kent.jacobs@canada.ca>
2020-02-03 15:28:24 -0500

    testing split method to generate new nid

--- src/stage_5/stage_5.py
+++ src/stage_5/stage_5.py
@@ -8,6 +8,7 @@ import subprocess
 import sys
 import zipfile
 from sqlalchemy import *
+from shapely.ops import split, snap, linemerge, unary_union
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 import helpers
@@ -67,6 +68,43 @@ class Stage:
         self.vintage_ferryseg = gpd.read_file("../../data/raw/vintage/NRN_RRN_NB_9_0_SHP/NRN_NB_9_0_SHP_en/4617/NRN_NB_9_0_FERRYSEG.shp")
         self.vintage_junction = gpd.read_file("../../data/raw/vintage/NRN_RRN_NB_9_0_SHP/NRN_NB_9_0_SHP_en/4617/NRN_NB_9_0_JUNCTION.shp")
 
+    def split_line_by_nearest_points(self, gdf_line, gdf_points, tolerance):
+        """
+        Split the union of lines with the union of points resulting
+        Parameters
+        ----------
+        gdf_line : geoDataFrame
+            geodataframe with multiple rows of connecting line segments
+        gdf_points : geoDataFrame
+            geodataframe with multiple rows of single points
+
+        Returns
+        -------
+        gdf_segments : geoDataFrame
+            geodataframe of segments
+        """
+
+        gdf_line["diss"] = "1"
+        line = gdf_line.dissolve(by="diss")
+
+        # union all geometries
+        line = gdf_line.geometry.unary_union
+        line = linemerge(line)
+        coords = gdf_points.geometry.unary_union
+
+        # snap and split coords on line
+        # returns GeometryCollection
+        split_line = split(line, snap(coords, line, tolerance))
+
+        # transform Geometry Collection to GeoDataFrame
+        segments = [feature for feature in split_line]
+
+        gdf_segments = gpd.GeoDataFrame(
+            list(range(len(segments))), geometry=segments)
+        gdf_segments.columns = ['index', 'geometry']
+
+        return gdf_segments
+
     def roadseg_equality(self):
         """Checks if roadseg features have equal geometry."""
 
@@ -87,6 +125,7 @@ class Stage:
 
                 # Apply NID from latest vintage to newest data.
                 self.dframes["roadseg"]["nid"] = self.vintage_roadseg["nid"]
+                # self.dframes["roadseg"]["nid"] = "Equal"
 
             else:
 
@@ -94,16 +133,20 @@ class Stage:
                 if row["equals"] == 0:
                     logger.warning("Unequal roadseg geometry detected for uuid: {}".format(index))
 
+                    self.dframes["roadseg"]["nid"] = ""
+
                     # This is temporary. The same NID will have to be applied to segments between junctions.
-                    self.dframes["roadseg"]["nid"] = [uuid.uuid4().hex for _ in range(len(self.dframes["roadseg"]))]
+                    # self.dframes["roadseg"]["nid"] = [uuid.uuid4().hex for _ in range(len(self.dframes["roadseg"]))]
 
         # self.nb_new.to_file("../../data/interim/nb_test.gpkg", layer="test", driver="GPKG")
-        print(self.dframes["roadseg"]["nid"])
-        sys.exit(1)
 
+        split_lines = self.split_line_by_nearest_points(self.dframes["roadseg"], self.dframes["junction"], tolerance=0.5)
 
         logger.info("Writing test road segment GPKG.")
         helpers.export_gpkg({"roadseg_equal": self.dframes["roadseg"]}, self.data_path)
+        helpers.export_gpkg({"split_lines": split_lines}, self.data_path)
+
+        sys.exit(1)
 
     def ferryseg_equality(self):
         """Checks if ferryseg features have equal geometry."""
