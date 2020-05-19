2865304bd5009ff3c48c1522e9593f0b96401f92
Reg Maltais <reginald.maltais@gmail.com>
2020-05-19 12:56:41 -0400

    Define which layers are attribute or spatial

--- geobasenrn/__init__.py
+++ geobasenrn/__init__.py
@@ -15,3 +15,6 @@ DEFAULT_PRECISION = 7
 
 # EPSG code to be used for all GeoBase data
 SRS_EPSG_CODE = 4617
+
+spatial_layers = ['blkpassage', 'junction', 'tollpoint', 'ferryseg', 'roadseg']
+attribute_layers = ['addrange', 'altnamlink', 'strplaname']
