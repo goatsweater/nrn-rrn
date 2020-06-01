54826def899a96a15e68c8e10d0a4a11afa739b4
Reg Maltais <reginald.maltais@gmail.com>
2020-06-01 08:10:10 -0400

    Indicate NRN version spec.

--- geobasenrn/__init__.py
+++ geobasenrn/__init__.py
@@ -9,6 +9,7 @@ import geobasenrn as nrn
 ```
 
 """
+__spec_version__ = 2.0
 
 # Default precision for coordinates
 DEFAULT_PRECISION = 7
@@ -16,5 +17,6 @@ DEFAULT_PRECISION = 7
 # EPSG code to be used for all GeoBase data
 SRS_EPSG_CODE = 4617
 
+# Define the layers in the dataset
 spatial_layers = ['blkpassage', 'junction', 'tollpoint', 'ferryseg', 'roadseg']
 attribute_layers = ['addrange', 'altnamlink', 'strplaname']
